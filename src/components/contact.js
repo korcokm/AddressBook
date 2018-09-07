a049cca52e67673e475a3992662527efb5b04a5f
Mike Korcok <korcokm@Mikes-MacBook-Pro.local>
2018-09-07 14:21:28 -0700

    new code

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,6 +5,12 @@ import './contact.css';
 export default function Contact(props){
     return 
     (
+        <img className="contact=photo">
 
+        </img>
+        <h2 className="contact-name"></h2>
+        <div className="contact-address"></div>
+
+        
     );
 };
